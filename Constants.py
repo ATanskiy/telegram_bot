e9ed1c00984cfcb83690ed3785f8a6512e8e3bbf
atans <atanskiy1989@gmail.com>
2024-02-25 00:03:45 +0200

    new commit

---
+++ Constants.py
@@ -0,0 +1,15 @@
+API_KEY = '6800194127:AAFO68qbu786vOVEkHD2cQB5CLiToISElzs'
+BOT_NAME = '@Hamudnik_bot (https://t.me/Hamudnik_bot)'
+
+TMDB_API_KEY = '319f79413f808cebd0fd7d06450fb6f5'
+
+text_for_user_start = "Shalom aleihem ✡︎, welcome and bruchim habaim! Please use the '/help' command to get a list of all available services today. "
+text_for_user_continue = "Welcome back! ✡  Please use the '/help' command to get a list of all available services today. "
+
+list_of_commands = {
+        '/start': 'Start the bot and receive a welcome message.',
+        '/help': 'Display a list of available commands and their descriptions',
+        '/time': 'Get the current date and time',
+        '/quote': 'Receive a motivational quote for the day',
+        '/movies': 'Receive a list of latest movies released.'
+    }
