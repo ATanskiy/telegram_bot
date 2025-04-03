c9fb0c04bafaeaaeec2f4ebc1b05a5f5a3b24538
atans <atanskiy1989@gmail.com>
2025-04-03 16:51:39 +0300

    test

---
+++ Constants.py
@@ -0,0 +1,16 @@
+BOT_NAME = '@Hamudnik_bot (https://t.me/Hamudnik_bot)'
+
+text_for_user_start = "Shalom aleihem ✡︎, welcome and bruchim habaim! Please use the '/help' command to get a list of all available services today. "
+text_for_user_continue = "Welcome back! ✡  Please use the '/help' command to get a list of all available services today. "
+start_help_message = 'Shalom :) Here are the available commands and actions:\n\n'
+
+list_of_commands = {
+        '/start': 'Start the bot and receive a welcome message.',
+        '/help': 'Display a list of available commands and their descriptions',
+        '/time': 'Get the current date and time',
+        '/quote': 'Receive a motivational quote for the day',
+        '/movies': 'Receive a list of latest movies released.',
+        'some text': 'Please type any question you want or anything else'
+    }
+
+system_promt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.",
